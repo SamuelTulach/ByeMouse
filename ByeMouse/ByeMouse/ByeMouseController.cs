302b1c4ab27336aa43842a4a85fdd2e5a240e653
Samuel Tulach <samtulach@gmail.com>
2021-04-06 10:22:22 +0200

    first

---
+++ ByeMouse/ByeMouse/ByeMouseController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ByeMouse
+{
+    public class ByeMouseController : MonoBehaviour
+    {
+        public static ByeMouseController Instance { get; private set; }
+
+        public float HideAfterSeconds = 3f;
+        public float ThresholdInPixels = 3f;
+
+        private float _lastTime;
+        private Vector3 _lastMousePos;
+
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                GameObject.DestroyImmediate(this);
+                return;
+            }
+            GameObject.DontDestroyOnLoad(this);
+            Instance = this;
+        }
+
+        private void Start()
+        {
+            _lastTime = Time.timeSinceLevelLoad;
+            _lastMousePos = Input.mousePosition;
+        }
+
+        private void Update()
+        {
+            var dx = Input.mousePosition - _lastMousePos;
+            var move = (dx.sqrMagnitude > (ThresholdInPixels * ThresholdInPixels));
+            _lastMousePos = Input.mousePosition;
+
+            if (move)
+                _lastTime = Time.timeSinceLevelLoad;
+
+            Cursor.visible = (Time.timeSinceLevelLoad - _lastTime) < HideAfterSeconds;
+        }
+
+        private void OnDestroy()
+        {
+            if (Instance == this)
+                Instance = null;
+        }
+    }
+}
---
+++ ByeMouse/ByeMouse/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using IPA;
+using IPA.Config;
+using IPA.Config.Stores;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using IPALogger = IPA.Logging.Logger;
+
+namespace ByeMouse
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+
+        [Init]
+        public void Init(IPALogger logger)
+        {
+            Instance = this;
+            Log = logger;
+        }
+
+        [OnStart]
+        public void OnApplicationStart()
+        {
+            new GameObject("ByeMouseController").AddComponent<ByeMouseController>();
+        }
+
+        [OnExit]
+        public void OnApplicationQuit()
+        {
+
+        }
+    }
+}
